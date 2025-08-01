69093eff852870de2b23e43c0382d73c187b219f
James Chen <jamesabc@me.com>
2025-08-01 14:23:49 -0700

    alias source table columns

--- models/staging/stg_tpch_orders.sql
+++ models/staging/stg_tpch_orders.sql
@@ -1,2 +1,11 @@
-SELECT *
+SELECT 
+o_orderkey AS order_key,
+o_custkey AS customer_key,
+o_orderstatus AS order_status,
+o_totalprice AS total_price,
+o_orderdate AS order_date,
+o_orderpriority AS order_priority,
+o_clerk AS clerk,
+o_shippriority AS ship_priority,
+o_comment AS comment
 FROM {{ source('tpch', 'orders') }}
